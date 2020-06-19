eac77f46aa4a60bdf29e42c316c992020800c01e
Dinakaran Sankaranarayanan <dinakaranonline@gmail.com>
2020-06-19 17:03:05 +0530

    Initial Commit

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response, NextFunction } from "express";
+
+import { json } from "body-parser";
+
+import toDoRoutes from "./routes/todo";
+
+const app = express();
+
+app.use("/todos", toDoRoutes);
+
+
+app.use(json());
+
+app.use(
+  (
+    err: Error,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    res.status(500).json({ message: err.message });
+  }
+);
+
+app.listen(3000);
